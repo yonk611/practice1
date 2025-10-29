4de83fac1752c52100b68352389ab78716a5c142
yonk611 <wongok.s240471@ggh.goe.go.kr>
2025-10-29 19:08:26 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import pandas as pd
 # https://raw.githubusercontent.com/huhshin/streamlit/master/data_iris.csv 읽고 나타내기 
 def main_page():
     st.header('Main Page')
-    st.image('https://images.pexels.com/photos/5677011/pexels-photo-5677011.jpeg?auto=compress&cs=tinysrgb&w=200')
+    st.image('https://t1.daumcdn.net/cfile/tistory/99B49F3D5AF85F1F07')
     iris = pd.read_csv('https://raw.githubusercontent.com/huhshin/streamlit/master/data_iris.csv')
     st.write(iris)
     
